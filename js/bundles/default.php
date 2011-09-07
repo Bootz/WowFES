38ceb76879f2885e8ef0f97cb2d1a5c692c89a88
Yi-Feng Tzeng <yftzeng@gmail.com>
2011-09-07 12:16:01 +0800

    bugfix: remove dobule '/' in bundles/default.php
    Signed-off-by: Yi-Feng Tzeng <yftzeng@gmail.com>

--- js/bundles/default.php
+++ js/bundles/default.php
@@ -4,7 +4,5 @@ echo "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min
 echo "<script>!window.jQuery && document.write('<script src=\"http://code.jquery.com/jquery-1.5.min.js\"><\/script>');</script>\n";
 
 $this->registry->js->javascript_tag('framebuster.js');
-//$this->registry->js->javascript_tag('vendor/jquery-1.4.4.min.js');
-//$this->registry->js->javascript_tag('vendor/jquery-1.5.0.min.js');
-$this->registry->js->javascript_tag('/vendor/dialog_box/dialog_box.js');
+$this->registry->js->javascript_tag('vendor/dialog_box/dialog_box.js');
 ?>
