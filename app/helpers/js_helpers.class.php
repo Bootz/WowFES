f5f509bec94628938e0ba66955c532d4ded4b9cf
Yi-Feng Tzeng <yftzeng@gmail.com>
2011-09-19 11:51:20 +0800

    bugfix: fix js helpers
    Signed-off-by: Yi-Feng Tzeng <yftzeng@gmail.com>

--- app/helpers/js_helpers.class.php
+++ app/helpers/js_helpers.class.php
@@ -1,13 +1,16 @@
 <?php
 class js_helpers extends base_helpers {
     public function javascript_tag($url, $config = array()) {
-        if (strpos($url, '/vendor/') === 0) {
-            if (__site !== '') {
-                $url = __site . '/' . $url;
-            }
+        $site = '';
+        if ((array_key_exists('secure', $config) && $config['secure'] == TRUE)) {
+            $site = 'https://' . __fqdn;
         }
-        else if (!strstr($url, '//')) {
-            $url = __site . '/js/' . $url;
+
+        if (strpos($url, '/vendor/') === 0) {
+            $url = $site . __site . $url;
+        }
+        else {
+            $url = $site . __site . '/js/' . $url;
         }
 
         if (!(array_key_exists('cache', $config) && $config['cache'] == TRUE)) {
